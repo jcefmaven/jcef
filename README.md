fc3766af57bc8bbbb6d398bb11966ed31835f77c
FriwiDev <friwidev@gmail.com>
2020-11-14 21:03:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# CEF Java Bindings Maven Wrapper #
+
+This package combines the JCEF API with the Maven JCEF Loader to provide a full JCEF Maven
+setup without natives. This repository does not contain JCEF itself.
+Please refer to the official JCEF repositories for JCEF sources.
+To get information on how to use this package, visit the main project page:
+https://github.com/jcefmaven/jcefmaven
---
+++ src/main/java/org/cef/CefVersion.java
@@ -0,0 +1,38 @@
+package org.cef;
+
+/**
+ * This class provides information about the installed JCEF version.<br/>
+ * <br/>
+ * JCef Version: {version}<br/>
+ * Cef Version: {native_version}<br/>
+ * JCef git commit reference: {ref}
+ * @author Fritz Windisch
+ */
+public class CefVersion {
+    /**
+     * Retrieves the current Java Binding version.<br/>
+     * Current value: {version}
+     * @return Java Binding Version
+     */
+    public static String getJCefVersion(){
+        return "{version}";
+    }
+
+    /**
+     * Retrieves the current version of the bundled natives.<br/>
+     * Current value: {native_version}
+     * @return Native version
+     */
+    public static String getNativeVersion(){
+        return "{native_version}";
+    }
+
+    /**
+     * Retrieves the current Java Binding git commit id.<br/>
+     * Current value: {ref}
+     * @return Java Binding git commit reference
+     */
+    public static String getJCefRef(){
+        return "{ref}";
+    }
+}
